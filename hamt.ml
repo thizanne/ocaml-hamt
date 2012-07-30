ab2f18df7c49526f48a1e46aa59e2ddf8a7bc406
Thibault Suzanne <thi.suzanne@gmail.com>
2012-07-30 19:36:33 +0200

    Should be complete regarding functionalities now

--- hamt.ml
+++ hamt.ml
@@ -13,7 +13,8 @@ type ('k, 'v) t =
 let rec show pk pv = function
   | Empty -> ()
   | Leaf (_, k, v) -> printf "(%s, %s) " (pk k) (pv v)
-  | HashCollision (_, li) -> List.iter (fun (k, v) -> printf "(%s, %s)" (pk k) (pv v)) li
+  | HashCollision (_, pairs) -> 
+      List.iter (fun (k, v) -> printf "(%s, %s)" (pk k) (pv v)) pairs
   | BitmapIndexedNode (_, arr) -> Array.iter (show pk pv) arr
   | ArrayNode (_, arr) -> Array.iter (show pk pv) arr
 
@@ -21,7 +22,7 @@ let print = show (sprintf "%d") (sprintf "%d")
 
 let empty = Empty
 let leaf h k v = Leaf (h, k, v)
-let hash_collision h li = HashCollision (h, li)
+let hash_collision h pairs = HashCollision (h, pairs)
 let bitmap_indexed_node bitmap base = BitmapIndexedNode (bitmap, base)
 let array_node num_children children = ArrayNode (num_children, children)
 
@@ -32,7 +33,7 @@ let is_empty x = x = Empty
 let rec cardinal = function
   | Empty -> 0
   | Leaf (_, _, _) -> 0
-  | HashCollision (_, li) -> List.length li
+  | HashCollision (_, pairs) -> List.length pairs
   | BitmapIndexedNode (_, base) -> Array.fold_left (fun acc child -> acc + cardinal child) 0 base
   | ArrayNode (_, children) -> Array.fold_left (fun acc child -> acc + cardinal child) 0 children
 
@@ -113,16 +114,16 @@ let expand_bitmap_node sub_hash node bitmap sub_nodes =
   tab.(sub_hash) <- node;
   array_node n tab
     
-let pack_array_node sub_hash_to_remove num_children children =
+let pack_array_node to_remove num_children children =
   let base = Array.make num_children Empty in
   let rec loop ix jx bitmap =
     if ix = chunk then bitmap_indexed_node bitmap base
     else if
-        is_empty children.(ix) || ix = sub_hash_to_remove
-    then loop (ix + 1) jx bitmap
+        is_empty children.(ix) || to_remove ix
+    then loop (succ ix) jx bitmap
     else begin
       base.(jx) <- children.(ix);
-      loop (ix + 1) (jx + 1) (bitmap lor (1 lsl ix))
+      loop (succ ix) (succ jx) (bitmap lor (1 lsl ix))
     end
   in loop 0 0 0
 
@@ -188,11 +189,12 @@ let rec alter_node shift update hash key = function
       let child' = alter_node (shift + shift_step) update hash key child in
       match change (is_empty child) (is_empty child') with
         | Nil -> arr_node
-        | Removed -> array_node (num_children - 1) (set children sub_hash Empty)
+        | Added -> array_node (succ num_children) (set children sub_hash Empty)
         | Modified -> array_node num_children (set children sub_hash child')
-        | Added ->
-            if num_children = arrnode_min then pack_array_node sub_hash num_children children
-            else array_node (num_children + 1) (set children sub_hash child')
+        | Removed ->
+            if num_children = arrnode_min then 
+              pack_array_node (( = ) sub_hash) num_children children
+            else array_node (num_children - 1) (set children sub_hash child')
               
 let alter update key root =
   alter_node 0 update (hash key) key root
@@ -210,27 +212,122 @@ let modify _k f = alter (function | None -> None | Some v -> Some (f v))
 
 let modify_def v0 _k f = alter (function | None -> Some (f v0) | Some v -> Some (f v))
 
-let rec expand_bitmap_node f indices base = 
-  match (indices, base) with
-    | [], [] -> [], []
-    | i :: is, b :: bs -> 
-        begin
-          match f b with
-            | None -> expand_bitmap_node is bs
-            | Some v -> expand_bitmap_node 
-        
-let rec alter_all f = function
-  | Empty -> Empty
-  | Leaf (h, k, v) -> option None (leaf h k) (f v)
-  | HashCollision (h, li) -> 
+let rec alter_hc f = function
+  | [] -> []
+  | (k, v) :: xs -> 
       begin
-        match List.map (fun (k, v) -> (k, f v)) li with
+        match f k v with
+          | None -> alter_hc f xs
+          | Some w -> (k, w) :: alter_hc f xs
+      end
+
+and alter_bmnode f indices base =
+  let rec aux n = function
+    | [] -> [], []
+    | i :: is ->
+        begin
+          match alter_all f base.(n) with
+            | Empty -> aux (succ n) is
+            | x -> 
+                let (iss, bss) = aux (succ n) is in
+                (i :: iss, x :: bss)
+        end
+  in aux 0 indices
+  
+and alter_all f = function
+  | Empty -> Empty
+  | Leaf (h, k, v) -> option Empty (leaf h k) (f k v)
+  | HashCollision (h, pairs) -> 
+      begin
+        match alter_hc f pairs with
           | [] -> Empty
           | [(k, v)] -> Leaf (h, k, v)
-          | _ as li' -> HashCollision (h, li')
+          | pairs' -> HashCollision (h, pairs')
       end
   | BitmapIndexedNode (bitmap, base) ->
-      BitmapIndexedNode (bitmap, Array.map (mapi f) base)
+      begin
+        match alter_bmnode f (bitmap_to_indices bitmap) base with
+          | _, [] -> Empty
+          | _, [x] when is_tip_node x -> x
+          | indices, base ->
+              BitmapIndexedNode (indices_to_bitmap indices, (Array.of_list base))
+      end
   | ArrayNode (num_children, children) ->
-      ArrayNode (num_children, Array.map (mapi f) children)
-    
+      let children = Array.map (alter_all f) children in
+      let num_children = Array.fold_left (fun n v -> if v = Empty then n else succ n) 0 children in
+      if num_children < arrnode_min 
+      then pack_array_node (fun _ -> false) num_children children
+      else ArrayNode (num_children, children)
+
+let map f = alter_all (fun _k v -> Some (f v))
+let filter f = alter_all (fun _k v -> if f v then Some v else None)
+
+let filter_map = alter_all
+
+let mapi f = alter_all (fun k v -> Some (f k v))
+let filteri f = alter_all (fun k v -> if f k v then Some v else None)
+
+let rec iter f = function
+  | Empty -> ()
+  | Leaf (_, k, v) -> f k v
+  | HashCollision (_, pairs) -> List.iter (fun (x, y) -> f x y) pairs
+  | BitmapIndexedNode (_, base) -> Array.iter (iter f) base
+  | ArrayNode (_, children) -> Array.iter (iter f) children
+
+let find key =
+  let rec find shift hash key = function
+    | Empty -> raise Not_found
+    | Leaf (_, k, v) -> if k = key then v else raise Not_found
+    | HashCollision (_, pairs) -> List.assoc key pairs
+    | BitmapIndexedNode (bitmap, base) ->
+        let sub_hash = hash_fragment shift hash in
+        let bit = to_bitmap sub_hash in
+        if bitmap land bit = 0 then raise Not_found
+        else find (shift + shift_step) hash key base.(from_bitmap bitmap sub_hash)
+    | ArrayNode (_, children) ->
+        let child = children.(hash_fragment shift hash) in
+        if is_empty child then raise Not_found
+        else find (shift + shift_step) hash key child
+  in find 0 (hash key) key
+                        
+let mem key hamt = 
+  try 
+    let _ = find key hamt in true
+  with 
+    | Not_found -> false
+
+let rec fold f hamt v0 =
+  match hamt with
+    | Empty -> v0
+    | Leaf (_, k, v) -> f k v v0
+    | HashCollision (_, pairs) -> 
+        List.fold_right (fun (k, v) acc -> f k v acc) pairs v0
+    | BitmapIndexedNode (_, base) ->
+        Array.fold_right (fold f) base v0
+    | ArrayNode (_, children) ->
+        Array.fold_right (fold f) children v0
+
+let to_assoc hamt = 
+  fold (fun k v acc -> (k, v) :: acc) hamt []
+
+let bindings = to_assoc
+
+let of_assoc hamt =
+  List.fold_left (fun acc (k, v) -> add k v acc) empty hamt
+
+let for_all f hamt = 
+  fold (fun k v acc -> f k v && acc) hamt true
+
+let exists f hamt =
+  fold (fun k v acc -> f k v || acc) hamt false
+
+let rec choose = function
+  | Empty -> raise Not_found
+  | Leaf (_, k, v) -> (k, v)
+  | HashCollision (_, li) -> List.hd li
+  | BitmapIndexedNode (_, base) -> choose base.(0)
+  | ArrayNode (_, children) ->
+      let rec loop n = 
+        if is_empty children.(n) then loop (succ n) else children.(n)
+      in choose (loop 0)
+      
