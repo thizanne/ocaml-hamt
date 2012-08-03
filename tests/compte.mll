5d72da2a96d7baccf66cccd18be53ef86f9ba200
Thibault Suzanne <thi.suzanne@gmail.com>
2012-08-03 16:26:42 +0200

    Tests added

---
+++ tests/compte.mll
@@ -0,0 +1,48 @@
+{
+(*
+  Usage: ./compte.native file.txt number_of_more_present_words_to_display
+*)
+
+(*
+module M = BatMap
+*)
+module M = Hamt
+}
+
+(* Wow.Ugly. Well, it does the job. *)
+let alpha = ['a' - 'z' 'A' - 'Z'] | "é" | "è" | "ù" | "à" | "ê" | "â" | "û" | "î" | "ç" | "À"
+
+rule word acc = parse
+  | eof { acc }
+  | alpha + as w { word (M.modify_def 0 w succ acc) lexbuf} 
+  | _ { word acc lexbuf }
+      
+{
+let rec inser k v = function
+  | [] -> [k, v]
+  | (k', v') :: tl as li -> 
+      if v <= v' then (k, v) :: li
+      else (k', v') :: (inser k v tl)
+        
+let inser_if k v li =
+  if v > snd (List.hd li)
+  then inser k v (List.tl li) else li
+
+let rec string_of_list = function
+  | [] -> ""
+  | (k, v) :: tl -> Printf.sprintf "%s [%d]\n%s" k v (string_of_list tl)
+
+let table = word M.empty (Lexing.from_channel (open_in Sys.argv.(1)));;
+let () =
+  let n_max = int_of_string (Sys.argv.(2)) in
+  Printf.printf "
+Nombre total de mots : %d
+Nombre de mots uniques : %d
+%d mots les plus présents : \n%s"
+    (M.fold ( + ) table 0)
+    (M.cardinal table)
+    n_max
+    (string_of_list
+       (M.foldi inser_if table (BatList.make n_max ("", 0))))
+}
+        
---
+++ tests/test.ml
@@ -0,0 +1,91 @@
+(*
+  This while file is ugly
+*)
+
+
+open Printf
+
+let () = Random.self_init ()
+
+module IntMap = Map.Make (struct type t = int let compare = compare end)
+
+let n = int_of_string (Sys.argv.(1))
+
+let random_int bound =
+  Int64.to_int (Random.int64 (Int64.of_int bound))
+
+let random min max = 
+  try random_int (max - min) + min
+  with Invalid_argument "Random.int64" -> min
+
+let generate =
+  let rec generate acc n =
+    if n = 0 then acc
+    else generate (random_int Pervasives.max_int :: acc) (pred n) 
+  in generate []
+  
+let generate_different li =
+  let rec aux acc = function
+    | [] -> acc
+    | [x] -> random x Pervasives.max_int :: acc
+    | x :: y :: zs -> aux (random x y :: acc) (y :: zs)
+  in aux [] (List.sort compare li)
+    
+(*
+let () = printf "Generating %d random integers\n%!" n
+let presents = List.sort compare (generate n)
+
+(* 
+   presents and absents might share elements, but it is highly unlikely 
+   and should have negligeable effect if so
+*)
+let () = printf "Generating %d other random integers\n%!" n
+let absents = List.sort compare (generate_different presents)
+*)
+
+(*
+let () = printf "\nInserting elements in a Hamt\n%!"
+let before = Sys.time ()
+let hamt = fill_structure Hamt.add Hamt.empty presents
+let () = printf "%f\n%!" (Sys.time () -. before)
+
+
+let () = printf "\nReading present elements in a Hamt\n%!"
+let before = Sys.time ()
+let () = List.iter (fun x -> ignore (Hamt.find x hamt)) presents
+let () = printf "%f\n%!" (Sys.time () -. before)
+
+let () = printf "\nReading absent elements in a Hamt\n%!"
+let before = Sys.time ()
+let () = List.iter (fun x -> try ignore (Hamt.find x hamt) with Not_found -> ()) absents
+let () = printf "%f\n%!" (Sys.time () -. before)
+*)
+
+let rec fill n acc =
+  if n = 0 then acc
+  else fill (pred n) (Hamt.add (random_int Pervasives.max_int) n acc)
+
+let () = printf "\nInserting elements in a IntMap\n%!"
+let before = Sys.time ()
+let map = fill n Hamt.empty
+let () = printf "%f\n%!" (Sys.time () -. before)
+
+let () = print_int (Hamt.cardinal map)
+
+(*
+let () = printf "\nReading present elements in a IntMap\n%!"
+let before = Sys.time ()
+let () = List.iter (fun x -> ignore (IntMap.find x map)) presents
+let () = printf "%f\n%!" (Sys.time () -. before)
+
+let () = printf "\nReading absent elements in a IntMap\n%!"
+let before = Sys.time ()
+let () = List.iter (fun x -> try ignore (IntMap.find x map) with Not_found -> ()) absents
+let () = printf "%f\n%!" (Sys.time () -. before)
+
+let () = printf "\nDeleting elements in a IntMap\n%!"
+let before = Sys.time ()
+let _ = List.fold_left (fun acc x -> IntMap.remove x acc) map presents
+let () = printf "%f\n%!" (Sys.time () -. before)
+
+*)
