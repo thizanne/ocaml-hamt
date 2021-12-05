a93d5d85653aec2b9871dff6b31f0cfefb1fd868
Rudi Grinberg <me@rgrinberg.com>
2021-12-05 11:06:49 -0700

    chore: write mli for BinUtils
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: 1DA62598-4B6D-44C5-8AAB-C79D1BB59299

--- lib/bitUtils.ml
+++ lib/bitUtils.ml
@@ -3,9 +3,6 @@ let sk3 = 0x33333333
 let skf0 = 0xf0f0f0f
 let skff = 0xff00ff
 
-(*
-  Given a bitmap, counts the number of one-bits
-*)
 let ctpop map =
   let map = map - (map lsr 1) land sk5 in
   let map = map land sk3 + (map lsr 2) land sk3 in
@@ -13,16 +10,10 @@ let ctpop map =
   in let map = map + map lsr 8
   in (map + map lsr 16) land 0x3f
 
-(*
-   Given a bitmap and a subhash, returns the index into the list
-*)
 let from_bitmap bitmap sub_hash =
   let mask = pred (1 lsl sub_hash) in
   ctpop (bitmap land mask)
 
-(*
-  Given a bitmap, returns the list of indices where a one-bit is present
-*)
 let bitmap_to_indices bitmap =
   let rec loop ix bitmap =
     if bitmap = 0 then []
@@ -31,16 +22,9 @@ let bitmap_to_indices bitmap =
       in if bitmap land 1 = 0 then r else ix :: r
   in loop 0 bitmap
 
-(*
-  Given a list of indices, returns the bitmap with one-bits at these indices
-*)
 let indices_to_bitmap =
   List.fold_left
     (fun x i -> x lor 1 lsl i) 0
 
-(*
-  Given a bitmap and an integer, returns the boolean corresponding to
-  the n-th bit (0 is the weakest) of the bitmap
-*)
 let nth_bit_set bitmap n =
   (bitmap asr n) land 1 = 1
