d53753bcc33b49520ffeca4f4a071c01c70b9303
Rudi Grinberg <me@rgrinberg.com>
2021-12-10 16:49:44 -0700

    refactor: imporve benchmarks for add
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: 57BE34ED-FA44-4189-9C36-E6F08C04F918

--- bench/bench.ml
+++ bench/bench.ml
@@ -1,29 +1,35 @@
 open Core_bench
-
-let max_rand = 1_000_000
-let () = Random.self_init ()
-
 module IntMap = Map.Make (Int)
 
+module Int_table = Caml.Hashtbl.Make (struct
+  include Caml.Int
+
+  let hash x = Caml.Hashtbl.hash x
+end)
+
 let rec fill n acc ~f =
-  match n with
-  | 0 -> acc
-  | _ -> fill (pred n) (f (Random.int max_rand) n acc) ~f
+  match n with 0 -> acc | _ -> fill (pred n) (f n n acc) ~f
 
 let () =
   let open Core in
-  let args = [ 1; 10; 100; 1000; 10000 ] in
+  let args = [ 1; 10; 100; 1000; 10_000 ] in
   Command.run
   @@ Bench.make_command
        [
          Bench.Test.create_indexed ~name:"IntMap.add" ~args (fun i ->
              Staged.stage @@ fun () ->
              ignore @@ fill i IntMap.empty ~f:IntMap.add);
-         Bench.Test.create_indexed ~name:"HashTbl.add" ~args (fun i ->
+         Bench.Test.create_indexed ~name:"Core - HashTbl.add" ~args (fun i ->
              Staged.stage @@ fun () ->
              ignore
              @@ fill i (Int.Table.create ()) ~f:(fun k v t ->
                     Int.Table.set t ~key:k ~data:v;
+                    t));
+         Bench.Test.create_indexed ~name:"Stdlib - HashTbl.add" ~args (fun i ->
+             Staged.stage @@ fun () ->
+             ignore
+             @@ fill i (Int_table.create 1) ~f:(fun k v t ->
+                    Int_table.add t k v;
                     t));
          Bench.Test.create_indexed ~name:"Hamt.Int.add" ~args (fun i ->
              Staged.stage @@ fun () ->
