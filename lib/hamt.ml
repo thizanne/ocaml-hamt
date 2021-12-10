f90793af08451a514535b82f6860079731e5e4ce
Rudi Grinberg <me@rgrinberg.com>
2021-12-10 15:38:43 -0700

    refactor: inline BitUtils module
    makes it easier to vendor
    
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: 3E5CBE5B-5F20-4079-A983-A62267B3E433

--- lib/hamt.ml
+++ lib/hamt.ml
@@ -1,3 +1,49 @@
+module BitUtils : sig
+  (* Given a bitmap, counts the number of one-bits *)
+  val ctpop : int -> int
+
+  (* Given a bitmap and a subhash, returns the index into the list *)
+  val from_bitmap : int -> int -> int
+
+  val nth_bit_set : int -> int -> bool
+  (** Given a bitmap and an integer, returns the boolean corresponding to
+  the n-th bit (0 is the weakest) of the bitmap *)
+
+  val indices_to_bitmap : int list -> int
+  (** Given a list of indices, returns the bitmap with one-bits at these indices *)
+
+  (* Given a bitmap, returns the list of indices where a one-bit is present *)
+  val bitmap_to_indices : int -> int list
+end = struct
+  let sk5 = 0x55555555
+  let sk3 = 0x33333333
+  let skf0 = 0xf0f0f0f
+
+  let ctpop map =
+    let map = map - ((map lsr 1) land sk5) in
+    let map = (map land sk3) + ((map lsr 2) land sk3) in
+    let map = (map land skf0) + ((map lsr 4) land skf0) in
+    let map = map + (map lsr 8) in
+    (map + (map lsr 16)) land 0x3f
+
+  let from_bitmap bitmap sub_hash =
+    let mask = pred (1 lsl sub_hash) in
+    ctpop (bitmap land mask)
+
+  let bitmap_to_indices bitmap =
+    let rec loop ix bitmap =
+      if bitmap = 0 then []
+      else if ix = 32 then []
+      else
+        let r = loop (succ ix) (bitmap asr 1) in
+        if bitmap land 1 = 0 then r else ix :: r
+    in
+    loop 0 bitmap
+
+  let indices_to_bitmap = List.fold_left (fun x i -> x lor (1 lsl i)) 0
+  let nth_bit_set bitmap n = (bitmap asr n) land 1 = 1
+end
+
 open BitUtils
 
 module type CONFIG = sig
