51c600be8e9a03bd3a46b89deae03d22803aac46
Rudi Grinberg <me@rgrinberg.com>
2021-12-10 17:59:45 -0700

    bench: test mem
    Signed-off-by: Rudi Grinberg <me@rgrinberg.com>
    
    ps-id: D0E33C4A-638D-4211-A433-606B422DB4A0

--- bench/bench.ml
+++ bench/bench.ml
@@ -10,9 +10,16 @@ end)
 let rec fill n acc ~f =
   match n with 0 -> acc | _ -> fill (pred n) (f n n acc) ~f
 
+let rec mem_all n ~f =
+  match n with
+  | -1 -> ()
+  | n ->
+      ignore (f n);
+      mem_all (pred n) ~f
+
 let () =
   let open Core in
-  let args = [ 1; 10; 100; 1000; 10_000 ] in
+  let args = [ 1; 10; 100; 1_000; 10_000 ] in
   Command.run
   @@ Bench.make_command
        [
@@ -34,4 +41,28 @@ let () =
          Bench.Test.create_indexed ~name:"Hamt.Int.add" ~args (fun i ->
              Staged.stage @@ fun () ->
              ignore @@ fill i Hamt.Int.empty ~f:Hamt.Int.add);
+         Bench.Test.create_indexed ~name:"IntMap.mem" ~args (fun i ->
+             let map = fill i IntMap.empty ~f:IntMap.add in
+             let f i = IntMap.mem i map in
+             Staged.stage @@ fun () -> mem_all i ~f);
+         Bench.Test.create_indexed ~name:"Core - HashTbl.mem" ~args (fun i ->
+             let map =
+               fill i (Int.Table.create ()) ~f:(fun k v t ->
+                   Int.Table.set t ~key:k ~data:v;
+                   t)
+             in
+             let f i = Int.Table.mem map i in
+             Staged.stage @@ fun () -> mem_all i ~f);
+         Bench.Test.create_indexed ~name:"Stdlib - HashTbl.mem" ~args (fun i ->
+             let map =
+               fill i (Int_table.create 1) ~f:(fun k v t ->
+                   Int_table.add t k v;
+                   t)
+             in
+             let f i = Int_table.mem map i in
+             Staged.stage @@ fun () -> mem_all i ~f);
+         Bench.Test.create_indexed ~name:"Hamt.Int.mem" ~args (fun i ->
+             let map = fill i Hamt.Int.empty ~f:Hamt.Int.add in
+             let f i = Hamt.Int.mem i map in
+             Staged.stage @@ fun () -> mem_all i ~f);
        ]
